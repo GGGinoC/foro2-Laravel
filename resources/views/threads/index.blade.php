677467c95fba0d25d88bfefd0629ed7138305f06
Gino Cornejo <gino.cornejo.p@gmail.com>
2020-05-25 16:45:45 -0400

    agregar formulario de respuesta

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Threads</div>
+                <div class="card-header">Hilos</div>
 
                 <div class="card-body">
                     @foreach ($threads as $thread)
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,6 +26,31 @@
         </div>
     </div>
 
+    <br>
+    @if (auth()->check())
+
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form action="{{ $thread->path() . '/replies' }}" method="post">
+                    {{ csrf_field() }}
+                    
+                    <div class="form-group">
+                       
+                        <textarea name="body" id="body" class="form-control" 
+                        placeholder="tienes algo que decir?" rows="4"></textarea>
+                        <button type="submit" class="btn btn-primary">Post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    
+    @else
+        <p class="text-center">
+            <a href="{{ route('login') }}">Ingresa</a> a tu cuenta para participar en este hilo
+        </p>
+    @endif
+
+
 
 </div>
 @endsection
