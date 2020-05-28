476d1f05de1f26bd17fe7ba167978a0f3b654cc9
Gino Cornejo <gino.cornejo.p@gmail.com>
2020-05-28 17:33:23 -0400

    agregar dropdown en navbar con canales

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,21 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <a class="nav-link" href="/threads">Discusiones</a>
+                        <li><a class="nav-link" href="/threads">Discusiones</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Canales
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                              @foreach (App\Channel::all() as $channel)
+                                
+                                <a class="dropdown-item" href="/threads/{{ $channel->slug }}">
+                                    {{ $channel->name }}
+                                </a>
+
+                              @endforeach
+                            </div>
+                          </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
