dab4e592db0dbfbbf7612dbae874adac7fc9950a
Gino Cornejo <gino.cornejo.p@gmail.com>
2020-05-28 17:32:30 -0400

    crear relacion entre canales e hilos + pruebas

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,9 @@ Route::get('/threads/{channel:slug}/{thread}', 'ThreadController@show');
 Route::post('/threads', 'ThreadController@store');
 Route::post('/threads/{channel:slug}/{thread}/replies', 'ReplyController@store');
 
+Route::get('/threads/{channel}', 'ThreadController@index');
+
+
+
 // Route::resource('threads', 'ThreadController');
 
